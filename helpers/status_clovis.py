f458463cb1a8e4e3a0225a11c78f1e7be0a9f198
VictorCyprien <victor.cyprien@limayrac.fr>
2023-03-08 11:24:47 +0100

    Add helpers

---
+++ helpers/status_clovis.py
@@ -0,0 +1,8 @@
+from discord import ActivityType
+
+status_clovis = [
+    [ActivityType.watching, "la tombe de Raspoutine"],
+    [ActivityType.listening, "le chant de Xivu Arath"],
+    [ActivityType.competing, "C'est mon choix"],
+    [ActivityType.playing, "Scènes de ménages avec Ana et Élisabeth"],
+]
